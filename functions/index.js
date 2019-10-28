d8ecff87a54111cb3f58690d566fef936450e6ac
Israel Ugbodaga <israelugbodaga@gmail.com>
2019-10-28 10:11:47 +0100

    first commit

---
+++ functions/index.js
@@ -0,0 +1,27 @@
+const functions = require('firebase-functions');
+const admin = require('firebase-admin');
+const uuidv5 = require('uuidv5')
+const cors = require('cors')({ origin: true });
+
+admin.initializeApp();
+
+exports.getUsers = functions.https.onRequest(async (req,res) => {
+	const eventref = admin.database().ref('/users');
+	const snapshot = await eventref.once('value');
+	const value = snapshot.val();
+	return res.send(200, value);
+})
+
+exports.addNewUser = functions.https.onRequest(async (req, res) => {
+	const user = req.body
+	const snapshot = await admin.database().ref("/users").push(user)
+	const value = snapshot;
+	return res.send(200, value)
+})
+
+exports.UserCreated = functions.database.ref("/users/{pushID}").onCreate((snapshot, context) => {
+    const pushID = context.params.pushID;
+		const user = snapshot.val();
+		const id = uuidv5("null", pushID);
+    return snapshot.ref.update({ id });
+  });
